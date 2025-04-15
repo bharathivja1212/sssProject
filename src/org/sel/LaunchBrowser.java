f6ea459135f9c6c08fa6fe364d5d5cfa7812d0cc
bharathivja1212 <bharathivja1212@gmail.com>
2025-04-15 12:13:16 +0530

    Hi! I'm going to update this project

---
+++ src/org/sel/LaunchBrowser.java
@@ -0,0 +1,16 @@
+package org.sel;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchBrowser {
+	
+	public static void main (String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\samson\\Downloads\\chromedriver-win64\\chromedriver-win64");
+		
+		WebDriver driver=new ChromeDriver();
+		
+	}
+
+}
